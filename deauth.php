bab2b55abd641415ec2ac6a697228ef28f74756b
Mark Coleman <mark@focorunner.com>
2012-04-08 10:57:57 -0600

    Update deauth.php

--- deauth.php
+++ deauth.php
@@ -2,6 +2,6 @@
 // Sets cookie expiry in the past, so browser destroys them and code.php then requires reauthentication
 setcookie("ctctauth","",time() - 3600);
 
-echo '<meta http-equiv="refresh" content="0;url=auth.php">';
+header('location: auth.php');
 
 ?>
